b92a7b13e823c7372d86bae8f1eb4a872b3f60b4
jobarv <jbarrera@motiontech.com.mx>
2023-05-10 19:55:09 -0600

    se agrega sweet alert

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -1,17 +1,30 @@
-Swal.fire({
-    title: '<strong>HTML <u>example</u></strong>',
+const modalFactura = () => {
+
+  Swal.fire({
+    title: '<strong>¿El Cliente ya está registrado?</strong>',
     icon: 'info',
-    html:
-      'You can use <b>bold text</b>, ' +
-      '<a href="//sweetalert2.github.io">links</a> ' +
-      'and other HTML tags',
+    // html:
+    //   'You can use <b>bold text</b>, ' +
+    //   '<a href="//sweetalert2.github.io">links</a> ' +
+    //   'and other HTML tags',
     showCloseButton: true,
     showCancelButton: true,
     focusConfirm: false,
     confirmButtonText:
-      '<i class="fa fa-thumbs-up"></i> Great!',
-    confirmButtonAriaLabel: 'Thumbs up, great!',
+      '<i class="fa fa-circle-check" style="color: #1dac1b;"></i>Si',
+    // confirmButtonAriaLabel: 'Thumbs up, great!',
     cancelButtonText:
-      '<i class="fa fa-thumbs-down"></i>',
+      '<i class="fa fa-thumbs-down"></i>No',
     cancelButtonAriaLabel: 'Thumbs down'
-  })
+  });
+}
+
+
+
+
+
+
+
+
+
+
